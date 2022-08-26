2e7b0958c980b3a63b62e8eb3b19cc98cdf41cfa
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-08-26 13:14:31 +1000

    changed structure of frog.jsx

---
+++ pages/cards/frog.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Frog(){
+    return(
+        <div>
+            <Head>
+                <title>e</title>
+            </Head>
+
+            <main>
+        
+            </main>
+        </div>
+
+    )
+}
