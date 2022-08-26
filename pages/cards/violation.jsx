434d7426796514b1b640f88195ae06660df5075f
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-08-26 13:15:56 +1000

    changed structure of frog.jsx

---
+++ pages/cards/violation.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Violation(){
+    return(
+        <div>
+            <p>violation</p>
+            <p>this totally works!</p>
+        </div>
+    )
+}
---
+++ pages/cards/warning.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Warning(){
+    return(
+        <div>
+            <p>Warning</p>
+            <p>this works works like really works</p>
+        </div>
+    )
+}
