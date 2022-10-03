0b3297c2f45f0b97739d94bde8cf8d7a92363b76
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-10-03 22:39:20 +1100

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import 'w3-css/w3.css'
 import '../styles/card.css'
 
 function MyApp({ Component, pageProps }) {
-  return(
-      <Component {...pageProps} />
-    )
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
--- pages/card.jsx
+++ pages/card.jsx
@@ -4,32 +4,21 @@ import Footer from '../components/footer'
 
 export default function Card() {
     return (
-        <div className="card-body">
+        <div className="page">
             <Head>
                 <title>ofbdhsagbhsj</title>
             </Head>
 
-            <div className="w3-display-middle card-bounds">
-                <div className="card-row-1">
-                    <div className="pfp-block">
-                        <img src="https://cdn.discordapp.com/avatars/644913030466895872/ff1dfe6b3d78297eeaa6856e4acaba70.png?size=1024"
-                            alt="picture" width="150" height="150" />
-                    </div>
-                    <div className="info-block w3-display-topright w3-right-align">
-                        <h2>TYSONLMAO</h2>
-                        <h4>put stuff
-                            <br />
-                            here
-                        </h4>
-                    </div>
-
-                    <p className="w3-display-bottommiddle w3-text-green">WORKING</p>
-                </div>
-                <div className="card-row-2 w3-display-middle">
+            <div className="page">
+                <div className="card-container">
+                    <section class="card-row">
+                        <p>hi</p>
+                        <img src="https://cdn.discordapp.com/avatars/644913030466895872/ff1dfe6b3d78297eeaa6856e4acaba70.png?size=1024" alt="a" className="card-image" />
+                    </section>
                 </div>
             </div>
 
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
