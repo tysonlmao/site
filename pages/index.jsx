e006aa1804e6b44eda014116f7d8ac6a7a763cb6
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-10-14 20:49:10 +1100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,9 +16,6 @@ export default function Home() {
         </header>
         <main className="main-body">
           {/* probably add content here */}
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/eMdMcsms68M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/eMdMcsms68M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/eMdMcsms68M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </main>
       <Footer/>
     </div>
