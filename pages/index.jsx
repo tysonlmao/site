48cef494456f96dbdbee1c5e365a90d110f787f8
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-10-24 05:54:34 +1100

    removed neofetch

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Footer from '../components/footer'
 import Nav from '../components/nav'
-import Neofetch from '../components/neofetch'
 
 export default function Home() {
   return (
@@ -12,13 +11,9 @@ export default function Home() {
       </Head>
 
       <Nav/>
-        <header className="">
-        </header>
         <main className="main-body">
           {/* <h2 className="main">welcome text</h2> */}
-          <Neofetch/>
         </main>
-      <Footer/>
     </div>
     
   )
