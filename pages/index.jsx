c2653df4fbb22a1b85ccb5f1ff2f2bcc5f5c1551
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-10-26 00:25:34 +1100

    temporary iframe

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,6 +13,7 @@ export default function Home() {
       <Nav/>
         <main className="main-body">
           {/* <h2 className="main">welcome text</h2> */}
+          <center><iframe className="iframe-temp" width="560" height="315" src="https://www.youtube.com/embed/pepjd7zwbtg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
         </main>
     </div>
     
