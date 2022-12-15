5e2b8786e4e945c00dfa7afef948b9f0e72c50be
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-12-15 19:38:11 +1100

    update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 }
 
-nextConfig
-module.exports = {
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
-}
+module.exports = nextConfig
