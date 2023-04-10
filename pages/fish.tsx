51453544ed971f8f8ec1635b10ba52fc78a94239
Tyson <83587481+tysonlmao@users.noreply.github.com>
2023-04-10 18:15:40 +1000

    added meta description for funky fish

--- pages/fish.tsx
+++ pages/fish.tsx
@@ -1,10 +1,16 @@
 import styles from "../styles/Fish.module.css";
+import Head from "next/head";
 export default function fish() {
     return (
     <>
-    <div className={styles.cover}>
-        
-    </div>
+    <Head>
+        <title>fash</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="funky fish" />
+        <meta property="og:url" content="https://tysonlmao.dev/fish" />
+        <meta property="og:image" content="https://media.tenor.com/6m3I1g_WiokAAAAM/fish-spin-sha.gif" />
+      </Head>
+    <div className={styles.cover}></div>
     </>
     )
 }
